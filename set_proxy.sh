4477a60da5b0c39eff8f5bc4e4ccbca94b2f5a07
lkudsurf <l8lssq3l7o@sjtu.edu.cn>
2025-05-04 15:35:19 +0000

    set proxy

--- set_proxy.sh
+++ set_proxy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-export http_proxy=http://127.0.0.1:7890
-export https_proxy=http://127.0.0.1:7890
+git config --global http.proxy http://127.0.0.1:7890
+git config --global https.proxy http://127.0.0.1:7890
